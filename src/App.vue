8daa12a8a6968a41410a784a9174c706d366738a
Martin Corredor <dev.martincorredor@gmail.com>
2025-01-17 16:19:53 -0500

    ADD. Add initial project files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const name = 'Nombresito dinámico';
+const stylecolor = 'color: blue';
+</script>
+
+<template>
+  <h1>Holaaa {{ name }}</h1>
+  <h2 :style="stylecolor">Soy azul</h2>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
